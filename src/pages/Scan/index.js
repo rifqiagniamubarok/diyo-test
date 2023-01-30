35be3a4bb60e0c01c32322beb56179c6648f2b03
Rifqi Agnia Mubarok <95258391+rifqiagniamubarok@users.noreply.github.com>
2023-01-30 23:04:13 +0700

    udpatess

--- src/pages/Scan/index.js
+++ src/pages/Scan/index.js
@@ -1,29 +1,30 @@
-import { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+import { useEffect, useState } from 'react';
+import { useHtml5QrCodeScanner } from 'react-html5-qrcode-reader';
 
 const Scan = () => {
   const [res, setRes] = useState('');
-  const handleScan = (data) => {
-    console.log(data);
-  };
-  const handleError = (error) => {
-    console.error(error);
-  };
-  console.log(res);
+  const { Html5QrcodeScanner } = useHtml5QrCodeScanner(
+    'url to the .min.js (see examples).'
+  );
+
+  useEffect(() => {
+    if (Html5QrcodeScanner) {
+      // Creates anew instance of `HtmlQrcodeScanner` and renders the block.
+      let html5QrcodeScanner = new Html5QrcodeScanner(
+        'reader',
+        { fps: 10, qrbox: { width: 250, height: 250 } },
+        /* verbose= */ false
+      );
+      html5QrcodeScanner.render(
+        (data: any) => console.log('success ->', data),
+        (err: any) => console.log('err ->', err)
+      );
+    }
+  }, [Html5QrcodeScanner]);
   return (
     <div>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setRes(result?.text);
-          }
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      />
       <p>hello</p>
+      <div id="reader">ssd</div>
     </div>
   );
 };

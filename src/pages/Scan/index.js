7d72deacb99c78ac102c6bd2ed144b54d9992d0b
Rifqi Agnia Mubarok <95258391+rifqiagniamubarok@users.noreply.github.com>
2023-01-30 22:52:44 +0700

    udpatess

--- src/pages/Scan/index.js
+++ src/pages/Scan/index.js
@@ -1,4 +1,30 @@
+import { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+
 const Scan = () => {
-  return <div>Scan</div>;
+  const [res, setRes] = useState('');
+  const handleScan = (data) => {
+    console.log(data);
+  };
+  const handleError = (error) => {
+    console.error(error);
+  };
+  console.log(res);
+  return (
+    <div>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setRes(result?.text);
+          }
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>hello</p>
+    </div>
+  );
 };
 export default Scan;

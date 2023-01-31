5dc0ba743eb31dc22dc6b5bca0e6e1fe33831f3a
Rifqi Agnia Mubarok <95258391+rifqiagniamubarok@users.noreply.github.com>
2023-01-31 20:06:15 +0700

    updatess

--- src/pages/Scan/index.js
+++ src/pages/Scan/index.js
@@ -1,26 +1,7 @@
 import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
 
 const Scan = () => {
   const [data, setData] = useState('No result');
-  return (
-    <div>
-      <>
-        <QrReader
-          onResult={(result, error) => {
-            if (!!result) {
-              setData(result?.text);
-            }
-
-            if (!!error) {
-              console.info(error);
-            }
-          }}
-          style={{ width: '100%' }}
-        />
-        <p>{data}</p>
-      </>
-    </div>
-  );
+  return <div>scann</div>;
 };
 export default Scan;

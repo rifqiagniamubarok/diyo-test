030bf6f291b1dee81088e1319dc42d8cdb0f7996
Rifqi Agnia Mubarok <95258391+rifqiagniamubarok@users.noreply.github.com>
2023-02-01 14:01:06 +0700

    udpate fixed menu

--- src/pages/DetailMenu/index.js
+++ src/pages/DetailMenu/index.js
@@ -110,7 +110,7 @@ const DetailMenu = ({ dataMenu }) => {
       </div>
       {qty > 0 && (
         <div
-          className="bg-primary grid grid-cols-3  items-center left-4 right-4 bottom-4 px-4 py-2 rounded-lg absolute text-white"
+          className="bg-primary grid grid-cols-3  items-center left-4 right-4 bottom-4 px-4 py-2 rounded-lg fixed text-white"
           onClick={handleAddToBasket}
         >
           <div className="bg-primarydarker w-[40px] h-[34px] rounded-md flex justify-center items-center">
